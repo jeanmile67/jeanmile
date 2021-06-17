9e817c1458bacc88e5ca5a34ba0fb3ba03565172
Jean-Michel LE <jeanmile.le@protonmail.com>
2021-06-17 16:02:13 +0700

    ajout des images technos en local

--- src/components/Technology/Technology.jsx
+++ src/components/Technology/Technology.jsx
@@ -3,59 +3,16 @@ import { Col, Container, Row } from 'react-bootstrap';
 import PageTitle from '../PageTitle/PageTitle';
 import TechnologyWrapper, { LogoGrid } from './Technology.style';
 
-const logoTechnoData = [
-  {
-    id: 1,
-    name: 'scala',
-    image: 'https://www.vectorlogo.zone/logos/scala-lang/scala-lang-ar21.svg',
-  },
-  {
-    id: 2,
-    name: 'reactjs',
-    image: 'https://www.vectorlogo.zone/logos/reactjs/reactjs-ar21.svg',
-  },
-  {
-    id: 3,
-    name: 'java',
-    image: 'https://www.vectorlogo.zone/logos/java/java-horizontal.svg',
-  },
-  {
-    id: 4,
-    name: 'git',
-    image: 'https://www.vectorlogo.zone/logos/git-scm/git-scm-ar21.svg',
-  },
-  {
-    id: 5,
-    name: 'mongodb',
-    image: 'https://www.vectorlogo.zone/logos/mongodb/mongodb-ar21.svg',
-  },
-  {
-    id: 6,
-    name: 'apache kafka',
-    image: 'https://www.vectorlogo.zone/logos/apache_kafka/apache_kafka-ar21.svg',
-  },
-  {
-    id: 7,
-    name: 'gatsby',
-    image: 'https://www.vectorlogo.zone/logos/gatsbyjs/gatsbyjs-ar21.svg',
-  },
-  {
-    id: 8,
-    name: 'rasberryPi',
-    image: 'https://www.vectorlogo.zone/logos/raspberrypi/raspberrypi-ar21.svg',
-  },
-];
-
-const Technology = () => (
+const Technology = ({ technos }) => (
   <TechnologyWrapper id="about">
     <Container>
-      <PageTitle title="Technology" />
+      <PageTitle title="Technologies" />
       <Row>
-        {logoTechnoData.map((logoTechno) => (
-          <Col md={3} col={6} key={logoTechno.id}>
+        {technos.map((techno) => (
+          <Col md={3} col={6} key={techno.id}>
             <LogoGrid>
               <div className="inner">
-                <img src={logoTechno.image} alt={logoTechno.name} />
+                <img src={techno.image} alt={techno.name} />
               </div>
             </LogoGrid>
           </Col>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -46,6 +46,7 @@ export const homeQuery = graphql`
         }
       }
     }
+    technos: all
   }
 `;
 
